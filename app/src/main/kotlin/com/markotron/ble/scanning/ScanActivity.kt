9b178c0e5c836b3a438477d071919858af290f08
markotron <markotron@gmail.com>
2017-10-28 13:33:51 +0200

    Gradle update & still workin on a basic version

--- app/src/main/kotlin/com/markotron/ble/scanning/ScanActivity.kt
+++ app/src/main/kotlin/com/markotron/ble/scanning/ScanActivity.kt
@@ -1,184 +1,199 @@
 package com.markotron.ble.scanning
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.bluetooth.BluetoothAdapter
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.annotation.DrawableRes
-import android.support.annotation.StringRes
-import android.support.v4.content.ContextCompat
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.AttributeSet
-import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.LinearLayout.LayoutParams.MATCH_PARENT
-import android.widget.LinearLayout.LayoutParams.WRAP_CONTENT
+import android.util.Log
 import com.markotron.ble.BellaBleApp
-import com.markotron.ble.device.DeviceActivity
 import com.markotron.ble.R
-import com.polidea.rxandroidble.RxBleClient
+import com.markotron.ble.device.DeviceActivity
+import com.polidea.rxandroidble.RxBleClient.State.*
 import com.polidea.rxandroidble.scan.ScanResult
 import com.polidea.rxandroidble.scan.ScanSettings
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.scan_result_view.view.*
+import kotlinx.android.synthetic.main.activity_scanning.*
 import rx.Observable
-import rx.Subscription
+import rx.android.schedulers.AndroidSchedulers
+import rx.subjects.PublishSubject
+import rx.subjects.ReplaySubject
 import rx.subscriptions.CompositeSubscription
+import java.util.concurrent.TimeUnit
+
+sealed class State {
+  object Start : State()
+  object BluetoothNotAvailable : State()
+  object LocationPermissionNotGranted : State()
+  object BluetoothNotEnabled : State()
+  object LocationServicesNotEnabled : State()
+  data class BluetoothReady(val devices: List<ScanResult> = listOf()) : State()
+}
+
+sealed class Command {
+  object Refresh : Command()
+  data class NewScanResult(val scanResult: ScanResult) : Command()
+  data class SetBleState(val state: State) : Command()
+}
+
+class ScanViewModel(app: Application) : AndroidViewModel(app) {
+
+  private val bleClient = getApplication<BellaBleApp>().bleClient
+  private val devices = bleClient.scanBleDevices(ScanSettings.Builder().build())
+
+  private val commands = PublishSubject.create<Command>()
+  private val replay = ReplaySubject.createWithSize<State>(1)
+
+  // API
+  val state: Observable<State> = Observable
+      .merge(commands, scanningFeedback(replay), bleStateFeedback())
+      .doOnNext { Log.d("COMMAND", it.toString()) }
+      .scan<State>(State.Start) { state, command ->
+        when (command) {
+          is Command.Refresh ->
+            if (state is State.BluetoothReady) State.BluetoothReady()
+            else state
+          is Command.SetBleState -> command.state
+          is Command.NewScanResult ->
+            if (state is State.BluetoothReady)
+              State.BluetoothReady(updateScanResultList(state.devices, command.scanResult))
+            else state
+        }
+      }
+      .doOnNext { replay.onNext(it) }
+      .doOnNext { Log.d("STATE", it.toString()) }
+      .replay(1)
+      .refCount()
+
+  fun sendCommand(c: Command) = commands.onNext(c)
+
+  // FEEDBACKS
+  private fun scanningFeedback(state: Observable<State>) =
+      state
+          .map { it is State.BluetoothReady }
+          .distinctUntilChanged()
+          .switchMap {
+            if (it == true)
+              devices.filter { it.bleDevice.name != null }.onErrorResumeNext { Observable.empty() }
+            else Observable.empty()
+          }
+          .map { Command.NewScanResult(it) }
+
+  private fun bleStateFeedback() = bleClient
+      .observeStateChanges()
+      .startWith(bleClient.state)
+      .distinctUntilChanged()
+      .map {
+        when (it) {
+          READY -> Command.SetBleState(State.BluetoothReady(listOf()))
+          BLUETOOTH_NOT_AVAILABLE -> Command.SetBleState(State.BluetoothNotAvailable)
+          LOCATION_PERMISSION_NOT_GRANTED -> Command.SetBleState(State.LocationPermissionNotGranted)
+          LOCATION_SERVICES_NOT_ENABLED -> Command.SetBleState(State.LocationServicesNotEnabled)
+          BLUETOOTH_NOT_ENABLED -> Command.SetBleState(State.BluetoothNotEnabled)
+          null -> throw RuntimeException("The bluetooth state enum is null!")
+        }
+      }
+
+  // HELPERS
+  private fun updateScanResultList(currentResults: List<ScanResult>, newResult: ScanResult) =
+      currentResults
+          .minus(currentResults
+                     .filter { it.bleDevice.macAddress == newResult.bleDevice.macAddress })
+          .plus(newResult)
+          .sortedByDescending { it.rssi }
+
+}
 
 class ScanActivity : AppCompatActivity() {
 
-  lateinit var bleClient: RxBleClient
   lateinit var adapter: ScanResultAdapter
-  var refreshBluetoothList = false
+  lateinit var viewModel: ScanViewModel
 
   private val disposableBag = CompositeSubscription()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_scanning)
 
-    bleClient = (application as BellaBleApp).bleClient
+    viewModel = ViewModelProviders.of(this).get(ScanViewModel::class.java)
 
-    enableBluetooth()
-
-    adapter = ScanResultAdapter(this::connect).also {
-      disposableBag.add(it.observe(scannedDevices()))
-    }
+    adapter = ScanResultAdapter(this::openDeviceActivity)
     bluetooth_scan_result_rv.adapter = adapter
     bluetooth_scan_result_rv.layoutManager = LinearLayoutManager(this)
 
-    swipe_refresh_layout.setOnRefreshListener {
-      refreshBluetoothList = true
-    }
+    swipe_refresh_layout.setOnRefreshListener(this::onRefreshListener)
   }
 
-  private fun enableBluetooth() {
-    val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-    val REQUEST_ENABLE_BLUETOOTH = 1
-    startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
+  override fun onResume() {
+    super.onResume()
+    disposableBag.add(adapter.observe(scannedDevices()))
+
+    disposableBag.add(viewModel
+                          .state
+                          .observeOn(AndroidSchedulers.mainThread())
+                          .subscribe({
+                                       when (it) {
+                                         is State.BluetoothNotEnabled -> showBluetoothDisableSnackbar()
+                                         is State.BluetoothNotAvailable -> showBluetoothNotAvailableSnackbar()
+                                         is State.LocationPermissionNotGranted -> showLocationPermissionNotGrantedSnackbar()
+                                         is State.LocationServicesNotEnabled -> showLocationServiceNotEnabledSnackbar()
+                                       }
+                                     }, { it.printStackTrace() })
+    )
   }
 
-  private fun scannedDevices() =
-      bleClient
-          .scanBleDevices(ScanSettings.Builder().build())
-          .filter { it.bleDevice.name != null }
-          .scan<List<ScanResult>>(listOf()) { list, scanResult ->
-            if (refreshBluetoothList) {
-              refreshBluetoothList = false
-              swipe_refresh_layout.isRefreshing = false
-              listOf()
-            } else
-              list.minus(list.filter { it.bleDevice.macAddress == scanResult.bleDevice.macAddress })
-                  .plus(scanResult)
-                  .sortedByDescending { it.rssi }
-          }
-
-  override fun onDestroy() {
-    super.onDestroy()
+  override fun onPause() {
+    super.onPause()
     disposableBag.clear()
   }
 
-  private fun connect(macAddress: String) {
+  private fun scannedDevices() =
+      viewModel
+          .state
+          .filter { it is State.BluetoothReady }
+          .map {
+            (it as State.BluetoothReady)
+                .devices
+          }
+          .observeOn(AndroidSchedulers.mainThread())
 
+  private fun showBluetoothDisableSnackbar() {
+    shortSnackbar("Bluetooth is disabled!")
+  }
+
+  private fun showBluetoothNotAvailableSnackbar() {
+    shortSnackbar("Bluetooth not available!")
+  }
+
+  private fun showLocationPermissionNotGrantedSnackbar() {
+    shortSnackbar("Location permission not granted!")
+  }
+
+  private fun showLocationServiceNotEnabledSnackbar() {
+    shortSnackbar("Location service not enabled!")
+  }
+
+  private fun openDeviceActivity(macAddress: String) {
     val intent = Intent(this, DeviceActivity::class.java)
     intent.putExtra("macAddress", macAddress)
     startActivity(intent)
   }
 
-}
-
-class ScanResultView @JvmOverloads constructor(context: Context,
-                                               attrs: AttributeSet? = null
-) : LinearLayout(context, attrs) {
-
-  init {
-    inflate(context, R.layout.scan_result_view, this)
-    layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-    orientation = LinearLayout.HORIZONTAL
-
-    attrs?.let {
-      context.obtainStyledAttributes(it, R.styleable.ScanResultView, 0, 0).apply {
-        getResourceId(R.styleable.ScanResultView_srv_label, -1).let {
-          if (it != -1) setLabel(it)
-        }
-        getString(R.styleable.ScanResultView_srv_label).let {
-          if (it != null) setLabel(it)
-        }
-        getResourceId(R.styleable.ScanResultView_srv_image, -1).let {
-          if (it != -1) setImage(it)
-        }
-      }.recycle()
-    }
+  private fun onRefreshListener() {
+    viewModel.sendCommand(Command.Refresh)
+    Observable.timer(2, TimeUnit.SECONDS)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe { swipe_refresh_layout.isRefreshing = false }
   }
 
-  fun setImage(@DrawableRes res: Int) {
-    device_image.setImageResource(res)
-  }
-
-  fun setLabel(label: String) {
-    device_name.text = label
-  }
-
-  fun setLabel(@StringRes label: Int) {
-    device_name.setText(label)
-  }
-
-  fun setRssi(rssi: Int) {
-    signal_strength.text = rssi.toString()
-  }
-
-  fun setMacAddress(macAddress: String) {
-    mac_address.text = macAddress
+  private fun shortSnackbar(message: String) {
+    Snackbar
+        .make(scanning_linear_layout, message, Snackbar.LENGTH_LONG)
+        .show()
   }
 
 }
 
-class ScanResultViewHolder(val view: ScanResultView) : RecyclerView.ViewHolder(view)
-
-class ScanResultAdapter(val onClick: (String) -> Unit) : RecyclerView.Adapter<ScanResultViewHolder>() {
-
-  private var data: List<ScanResult> = listOf()
-
-  fun observe(observableData: Observable<List<ScanResult>>): Subscription =
-      observableData.subscribe {
-        data = it
-        notifyDataSetChanged()
-      }
-
-  fun clearData() {
-    data = listOf()
-    notifyDataSetChanged()
-  }
-
-  override fun getItemCount() = data.size
-
-  override fun onBindViewHolder(holder: ScanResultViewHolder, position: Int) {
-    val scanResult = data[position]
-    val name = scanResult.bleDevice.name ?: "No name"
-    holder.view.setLabel(name)
-    val icon = when {
-      name.startsWith("Spring") -> R.drawable.ic_sync_spring
-      name.startsWith("Leaf") -> R.drawable.ic_sync_leaf
-      else -> R.drawable.ic_devices_other_black_24dp
-    }
-    holder.view.setImage(icon)
-    holder.view.setRssi(scanResult.rssi)
-    holder.view.setMacAddress(scanResult.bleDevice.macAddress)
-    holder.view.setBackgroundColor(ContextCompat.getColor(holder.view.context,
-                                                          if (position % 2 == 0)
-                                                            R.color.lightBlue
-                                                          else
-                                                            R.color.white
-    ))
-    holder.view.setOnClickListener {
-      onClick(data[position].bleDevice.macAddress)
-    }
-  }
-
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-      ScanResultViewHolder(ScanResultView(
-          parent.context))
-
-}
---
+++ app/src/main/kotlin/com/markotron/ble/scanning/ScanResultAdapter.kt
@@ -0,0 +1,109 @@
+package com.markotron.ble.scanning
+
+import android.content.Context
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
+import com.markotron.ble.R
+import android.util.AttributeSet
+import android.view.ViewGroup
+import com.polidea.rxandroidble.scan.ScanResult
+import kotlinx.android.synthetic.main.view_scan_result.view.*
+import rx.Observable
+import rx.Subscription
+
+/**
+ * Created by markotron on 26/10/2017.
+ */
+class ScanResultView @JvmOverloads constructor(context: Context,
+                                               attrs: AttributeSet? = null
+) : LinearLayout(context, attrs) {
+
+  init {
+    inflate(context, R.layout.view_scan_result, this)
+    layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+    orientation = LinearLayout.HORIZONTAL
+
+    attrs?.let {
+      context.obtainStyledAttributes(it, R.styleable.ScanResultView, 0, 0).apply {
+        getResourceId(R.styleable.ScanResultView_srv_label, -1).let {
+          if (it != -1) setLabel(it)
+        }
+        getString(R.styleable.ScanResultView_srv_label).let {
+          if (it != null) setLabel(it)
+        }
+        getResourceId(R.styleable.ScanResultView_srv_image, -1).let {
+          if (it != -1) setImage(it)
+        }
+      }.recycle()
+    }
+  }
+
+  fun setImage(@DrawableRes res: Int) {
+    device_image.setImageResource(res)
+  }
+
+  fun setLabel(label: String) {
+    device_name.text = label
+  }
+
+  fun setLabel(@StringRes label: Int) {
+    device_name.setText(label)
+  }
+
+  fun setRssi(rssi: Int) {
+    signal_strength.text = rssi.toString()
+  }
+
+  fun setMacAddress(macAddress: String) {
+    mac_address.text = macAddress
+  }
+
+}
+
+class ScanResultViewHolder(val view: ScanResultView) : RecyclerView.ViewHolder(view)
+
+class ScanResultAdapter(val onClick: (String) -> Unit) : RecyclerView.Adapter<ScanResultViewHolder>() {
+
+  private var data: List<ScanResult> = listOf()
+
+  fun observe(observableData: Observable<List<ScanResult>>): Subscription =
+      observableData.subscribe({
+                                 data = it
+                                 notifyDataSetChanged()
+                               }, { it.printStackTrace() })
+
+  override fun getItemCount() = data.size
+
+  override fun onBindViewHolder(holder: ScanResultViewHolder, position: Int) {
+    val scanResult = data[position]
+    val name = scanResult.bleDevice.name ?: "No name"
+    holder.view.setLabel(name)
+    val icon = when {
+      name.startsWith("Spring") -> R.drawable.ic_sync_spring
+      name.startsWith("Leaf") -> R.drawable.ic_sync_leaf
+      else -> R.drawable.ic_devices_other_black_24dp
+    }
+    holder.view.setImage(icon)
+    holder.view.setRssi(scanResult.rssi)
+    holder.view.setMacAddress(scanResult.bleDevice.macAddress)
+    holder.view.setBackgroundColor(ContextCompat.getColor(holder.view.context,
+                                                          if (position % 2 == 0)
+                                                            R.color.lightBlue
+                                                          else
+                                                            R.color.white
+    ))
+    holder.view.setOnClickListener {
+      onClick(data[position].bleDevice.macAddress)
+    }
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+      ScanResultViewHolder(ScanResultView(
+          parent.context))
+
+}
