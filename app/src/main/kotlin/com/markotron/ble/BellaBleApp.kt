8860f93870533ed83051f08590e7f477a67f895e
markotron <markotron@gmail.com>
2017-10-30 10:23:18 +0100

    [Fabric] Fabric added (crashlytics and analytics)

--- app/src/main/kotlin/com/markotron/ble/BellaBleApp.kt
+++ app/src/main/kotlin/com/markotron/ble/BellaBleApp.kt
@@ -1,7 +1,9 @@
 package com.markotron.ble
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.polidea.rxandroidble.RxBleClient
+import io.fabric.sdk.android.Fabric
 
 /**
  * Created by markotron on 25/10/2017.
@@ -13,6 +15,8 @@ class BellaBleApp : Application() {
   override fun onCreate() {
     super.onCreate()
     bleClient = RxBleClient.create(this)
+
+    Fabric.with(this, Crashlytics())
   }
 
 }
